cc7cbc03006ba08845f110add6264ed23182e84f
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2023-10-12 13:26:48 +0700

    up perbaikan nomor  2

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts')
+@extends('products.layouts')
 
 @section('content')
 
@@ -6,25 +6,26 @@
     <div class="col-md-12">
 
         @if ($message = Session::get('success'))
-            <div class="alert alert-success" role="alert">
-                {{ $message }}
-            </div>
+        <div class="alert alert-success" role="alert">
+            {{ $message }}
+        </div>
         @endif
 
         <div class="card">
             <div class="card-header">Product List</div>
             <div class="card-body">
-                <a href="{{ route('products.create') }}" class="btn btn-success btn-sm my-2"><i class="bi bi-plus-circle"></i> Add New Product</a>
+                <a href="{{ route('products.create') }}" class="btn btn-success btn-sm my-2"><i
+                        class="bi bi-plus-circle"></i> Add New Product</a>
                 <table class="table table-striped table-bordered">
                     <thead>
-                      <tr>
-                        <th scope="col">S#</th>
-                        <th scope="col">Code</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Action</th>
-                      </tr>
+                        <tr>
+                            <th scope="col">S#</th>
+                            <th scope="col">Code</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Action</th>
+                        </tr>
                     </thead>
                     <tbody>
                         @forelse ($products as $product)
@@ -39,29 +40,34 @@
                                     @csrf
                                     @method('DELETE')
 
-                                    <a href="{{ route('products.show', $product->id) }}" class="btn btn-warning btn-sm"><i class="bi bi-eye"></i> Show</a>
+                                    <a href="{{ route('products.show', $product->id) }}"
+                                        class="btn btn-warning btn-sm"><i class="bi bi-eye"></i> Show</a>
 
-                                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary btn-sm"><i class="bi bi-pencil-square"></i> Edit</a>   
+                                    <a href="{{ route('products.edit', $product->id) }}"
+                                        class="btn btn-primary btn-sm"><i class="bi bi-pencil-square"></i> Edit</a>
 
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="bi bi-trash" onclick="return confirm('Do you want to delete this product?');"></i> Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Do you want to delete this product?');">
+                                        <i class="bi bi-trash"></i>
+                                        Delete</button>
                                 </form>
                             </td>
                         </tr>
                         @empty
-                            <td colspan="6">
-                                <span class="text-danger">
-                                    <strong>No Product Found!</strong>
-                                </span>
-                            </td>
+                        <td colspan="6">
+                            <span class="text-danger">
+                                <strong>No Product Found!</strong>
+                            </span>
+                        </td>
                         @endforelse
                     </tbody>
-                  </table>
+                </table>
 
-                  {{ $products->links() }}
+                {{ $products->links() }}
 
             </div>
         </div>
-    </div>    
+    </div>
 </div>
-    
+
 @endsection
